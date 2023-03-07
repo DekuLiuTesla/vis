dc5db36885a2672636ba772bbe4617f715872169
DekuLiuTesla <1369939686@qq.com>
2023-03-07 13:30:40 +0800

    Added font setting

--- vis_bar_sns.py
+++ vis_bar_sns.py
@@ -12,20 +12,22 @@ assert len(y_FSD) == len(y_FSF)
 df = pd.DataFrame({'FSD': y_FSD, 'FSF': y_FSF, 'delta': delta, 'labels': x_labels})
 
 # Drawing
+plt.rc('font', family='Times New Roman')
 sns.set_style("whitegrid")
 sns.barplot(df, x='labels', y='FSF', color='tomato', saturation=0.9, label='FSF')
-ax = sns.barplot(df, x='labels', y='FSD', color=(163/255, 218/255, 222/255), saturation=0.9, label='FSD')
+ax = sns.barplot(df, x='labels', y='FSD', color=(163 / 255, 218 / 255, 222 / 255), saturation=0.9, label='FSD')
 ax.axes.yaxis.set_ticklabels([])
 ax.spines['right'].set_visible(False)
 ax.spines['left'].set_visible(False)
-ax.bar_label(ax.containers[1], padding=-13, color='white')
+ax.bar_label(ax.containers[1], padding=-13, color='white', family='Times New Roman')
 for index, row in df.iterrows():
-    ax.text(row.name, row.FSF, '+'+str(round(row.delta, 2)), ha='center', position=(row.name, row.FSF+0.5))
-plt.legend(bbox_to_anchor=(0.4, 1))
-plt.title('mAP Comparison between FSD and FSF', fontweight='bold')
+    ax.text(row.name, row.FSF, '+' + str(round(row.delta, 2)), ha='center',
+            position=(row.name, row.FSF + 0.5), family='Times New Roman')
+plt.legend(bbox_to_anchor=(0.4, 1), prop='Times New Roman')
+plt.title('mAP Comparison between FSD and FSF', fontweight='bold', family='Times New Roman')
 plt.xlabel(None)
+plt.xticks(fontproperties='Times New Roman')
 plt.ylabel(None)
 plt.ylim((24, 95))
 plt.savefig("mAP.pdf", dpi=1024)
 plt.show()
-
